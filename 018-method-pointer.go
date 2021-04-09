22d4feecd13eb4d8fb0bacfe739d9a8ea87d9d62
Andrew Shitov <andy@shitov.ru>
2021-04-09 20:16:13 +0200

    Method with a pointer

---
+++ 018-method-pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Value struct {
+	X int
+}
+
+func (v Value) Inc() {
+	v.X++
+}
+
+func (v *Value) Inc2() {
+	v.X++
+}
+
+func main() {
+	a := Value{42}
+
+	a.Inc()
+	fmt.Println(a)
+
+	a.Inc2()
+	fmt.Println(a)
+}
